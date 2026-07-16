04b3ad8861367e0ec2cc3eb477357aa38d16d491
QuyQuy123 <quyhslc11@gmail.com>
2026-07-16 09:29:36 +0700

    test

---
+++ kidneyFailurePredictionSystem/src/main/java/com/kidneyFailurePredictionSystem/kidneyFailurePredictionSystem/KidneyFailurePredictionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kidneyFailurePredictionSystem.kidneyFailurePredictionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidneyFailurePredictionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidneyFailurePredictionSystemApplication.class, args);
+	}
+
+}
---
+++ kidneyFailurePredictionSystem/src/test/java/com/kidneyFailurePredictionSystem/kidneyFailurePredictionSystem/KidneyFailurePredictionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidneyFailurePredictionSystem.kidneyFailurePredictionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidneyFailurePredictionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
